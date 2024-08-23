74e3d4b42643a96502b7b9967e90e6b01697e2ae
espisepi <sepinaco@gmail.com>
2024-08-23 22:33:15 +0200

    hello world and 01-basic-types

---
+++ proyectos-fernando-herrera/Angular/01-typescript-intro/src/topics/01-basic-types.ts
@@ -0,0 +1,15 @@
+
+
+const name: string = 'Strider';
+
+// let hpPoints: number | string = 95
+let hpPoints: number | 'FULL' = 95;
+hpPoints = 'FULL';
+
+const isAlive: boolean = true;
+ 
+console.log({
+    name, hpPoints, isAlive
+})
+
+export {};
