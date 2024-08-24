5121d1cd9d289e46e2b10c6932193d5627e2bdcf
espisepi <sepinaco@gmail.com>
2024-08-24 17:02:36 +0200

    06-function-destructuring

---
+++ proyectos-fernando-herrera/Angular/01-typescript-intro/src/topics/06-functions-destructuring.ts
@@ -0,0 +1,82 @@
+
+interface Product {
+    description: string;
+    price: number;
+}
+
+const phone: Product = {
+    description: "Nokia A1",
+    price: 150.0
+}
+
+const tablet: Product = {
+    description: 'iPad Air',
+    price: 250.0
+}
+
+interface TaxCalculationOptions {
+    tax: number;
+    products: Product[];
+}
+
+// function taxCalculation( options: TaxCalculationOptions ): number[] {
+//     let total = 0;
+
+//     options.products.forEach( product => {
+//         // total = total + product.price;
+//         total += product.price;
+//     });
+
+//     return [ total, total * options.tax ];
+
+// }
+
+
+// const shoppingCart = [phone, tablet];
+// const tax = 0.15;
+
+// const result = taxCalculation({
+//     products: shoppingCart,
+//     tax: tax
+//     // tax // Similar a poner tax: tax
+// });
+
+// console.log( 'Total', result[0] );
+// console.log('Tax', result[1] );
+
+// Aplicar desestructuracion en todo el codigo anterior ==============================
+
+// function taxCalculation( options: TaxCalculationOptions ): [number, number] {
+// function taxCalculation( { tax, products }: TaxCalculationOptions ): [number, number] {
+function taxCalculation( options: TaxCalculationOptions ): [number, number] {
+    
+    const { tax, products } = options;
+    
+    let total = 0;
+
+    products.forEach( ({ price }) => {
+        // total = total + price;
+        total += price;
+    });
+
+    return [ total, total * tax ];
+
+}
+
+
+const shoppingCart = [phone, tablet];
+const tax = 0.15;
+
+const [ total, taxTotal ] = taxCalculation({
+    products: shoppingCart,
+    tax: tax
+    // tax // Similar a poner tax: tax
+});
+
+console.log( 'Total', total );
+console.log('Tax', taxTotal );
+
+
+
+
+export {};
