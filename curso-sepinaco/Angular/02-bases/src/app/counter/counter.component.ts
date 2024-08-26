57c78227f6475eb18b8039b8b10fc3e8b89da745
espisepi <sepinaco@gmail.com>
2024-08-26 14:22:53 +0200

    funcionalidad del contador

--- curso-sepinaco/Angular/02-bases/src/app/counter/counter.component.ts
+++ curso-sepinaco/Angular/02-bases/src/app/counter/counter.component.ts
@@ -4,8 +4,24 @@ import { Component } from "@angular/core";
 // @Component es un decorador que convierte la clase en un componente
 @Component({
     selector: 'app-counter',
-    template: '<h1>Hola Counter</h1>'
+    template: `
+      <h3>Counter: {{ counter }} </h3>
+      <button (click)="increaseBy(1)">+1</button>
+      <button (click)="resetCounter()">Reset</button>
+      <button (click)="increaseBy(-1)">-1</button>
+
+    `
 })
 export class CounterComponent {
+
+  public counter: number = 10;
+
+  increaseBy(value: number): void {
+    this.counter += value;
+  }
+
+  resetCounter() {
+    this.counter = 10;
+  }
 
 }
