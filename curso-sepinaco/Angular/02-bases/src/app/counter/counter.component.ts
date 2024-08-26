2684fbaf7c0f556c97547fbc68dc77f57f97681e
espisepi <sepinaco@gmail.com>
2024-08-26 14:09:29 +0200

    contador component

---
+++ curso-sepinaco/Angular/02-bases/src/app/counter/counter.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+
+// @Component es un decorador que convierte la clase en un componente
+@Component({
+    selector: 'app-counter',
+    template: '<h1>Hola Counter</h1>'
+})
+export class CounterComponent {
+
+}
