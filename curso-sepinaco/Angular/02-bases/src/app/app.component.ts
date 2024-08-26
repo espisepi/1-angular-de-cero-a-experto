aa4900b7a895d8be1e053d370a9700febe9a9a39
espisepi <sepinaco@gmail.com>
2024-08-26 14:03:53 +0200

    counter in app component

--- curso-sepinaco/Angular/02-bases/src/app/app.component.ts
+++ curso-sepinaco/Angular/02-bases/src/app/app.component.ts
@@ -2,9 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'bases';
+
+  public title: string = 'Hola Mundo';
+  public counter: number = 10;
+
+  increaseBy(value: number): void {
+    this.counter += value;
+  }
+
+  resetCounter() {
+    this.counter = 10;
+  }
+
 }
