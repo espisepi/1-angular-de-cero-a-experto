414889c0055dfa1ccbd801622dd559ad87ebb707
espisepi <sepinaco@gmail.com>
2024-08-28 13:16:56 +0200

    one way data binding

--- curso-sepinaco/Angular/02-bases/src/app/heroes/hero/hero.component.ts
+++ curso-sepinaco/Angular/02-bases/src/app/heroes/hero/hero.component.ts
@@ -11,4 +11,18 @@ export class HeroComponent {
   public name: string = 'ironman';
   public age: number = 45;
 
+  // Los getters se ven como una propiedad
+  // aunque en realidad es un metodo
+  // pero accedemos a ello como si fuesen propiedades
+  // Es decir, con interpolacion hariamos {{ capitalizedName }}
+  get capitalizedName(): string {
+    return this.name.toUpperCase();
+  }
+
+  // Esto es un metodo
+  // Con interpolacion hariamos {{ getHeroDescription() }}
+  getHeroDescription(): string {
+    return `${ this.name } - ${ this.age }`;
+  }
+
 }
