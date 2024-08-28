10662d7a23e3f1f9a04eb2414ef70db583edd586
espisepi <sepinaco@gmail.com>
2024-08-28 19:18:12 +0200

    @Output() - Emitir eventos al padre

--- curso-sepinaco/Angular/02-bases/src/app/dbz/components/add-character/add-character.component.ts
+++ curso-sepinaco/Angular/02-bases/src/app/dbz/components/add-character/add-character.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { Character } from '../../interfaces/character.interface';
 
 @Component({
@@ -8,6 +8,10 @@ import { Character } from '../../interfaces/character.interface';
 })
 export class AddCharacterComponent {
 
+
+  @Output()
+  public onNewCharacter: EventEmitter<Character> = new EventEmitter();
+
   public character: Character = {
     name: '',
     power: 0
@@ -15,6 +19,14 @@ export class AddCharacterComponent {
 
   emitCharacter(): void {
     console.log(this.character);
+
+    // mini-validacion
+    if( this.character.name.length === 0 ) return;
+
+    this.onNewCharacter.emit(this.character);
+
+    this.character.name = '';
+    this.character.power = 0;
   }
 
 }
--- curso-sepinaco/Angular/02-bases/src/app/dbz/pages/main-page.component.ts
+++ curso-sepinaco/Angular/02-bases/src/app/dbz/pages/main-page.component.ts
@@ -23,6 +23,9 @@ export class MainPageComponent {
     }
   ];
 
-  constructor() { }
+  onNewCharacter( character: Character ): void {
+    console.log('MainPage');
+    console.log(character);
+  }
 
 }
