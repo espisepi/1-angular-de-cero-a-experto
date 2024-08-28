81db1a744479d7bd7f8400ece29931d51c0808b5
espisepi <sepinaco@gmail.com>
2024-08-28 13:01:53 +0200

    interpolacion definition

--- curso-sepinaco/Angular/02-bases/src/app/heroes/hero/hero.component.ts
+++ curso-sepinaco/Angular/02-bases/src/app/heroes/hero/hero.component.ts
@@ -6,6 +6,8 @@ import { Component } from '@angular/core';
   styleUrl: './hero.component.css'
 })
 export class HeroComponent {
+  // Interpolacion es poner en el html
+  // estos atributos de esta manera: {{ name }} {{ age }}
   public name: string = 'ironman';
   public age: number = 45;
 
