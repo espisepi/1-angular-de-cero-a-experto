063f73077c40dc17a8b27a7140961bc6ca77597f
espisepi <sepinaco@gmail.com>
2024-08-28 18:30:04 +0200

    @Input() - Recibir del padre

--- curso-sepinaco/Angular/02-bases/src/app/dbz/components/list/list.component.ts
+++ curso-sepinaco/Angular/02-bases/src/app/dbz/components/list/list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
 
 @Component({
   selector: 'dbz-list',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrl: './list.component.css'
 })
 export class ListComponent {
+
+  // Input es un decorador que hace que le podamos pasar al ListComponent
+  // ese atributo, es decir,
+  // <dbz-list [characterList]="myCharacterList"></dbz-list>
+  @Input()
+  public characterList: Character[] = [
+    {
+      name: 'Trunk',
+      power: 10
+    }
+  ]
 
 }
